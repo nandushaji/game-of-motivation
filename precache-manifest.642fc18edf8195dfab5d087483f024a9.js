2aea1934fae2173cef11de939f48fd51398b02e7
Nandu-Shaji <nandu.s@keyvalue.systems>
2023-04-03 20:50:02 +0530

    Updates

---
+++ precache-manifest.642fc18edf8195dfab5d087483f024a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a456706c7d82098653a22ee4ab2c3086",
+    "url": "/game-of-motivation/index.html"
+  },
+  {
+    "revision": "620be6a900c5cc89e1fe",
+    "url": "/game-of-motivation/static/css/main.a3a627e3.chunk.css"
+  },
+  {
+    "revision": "5a0b763e6a60d36dcc7e",
+    "url": "/game-of-motivation/static/js/2.ba15851d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/game-of-motivation/static/js/2.ba15851d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "620be6a900c5cc89e1fe",
+    "url": "/game-of-motivation/static/js/main.07ca87ee.chunk.js"
+  },
+  {
+    "revision": "c96e173a10b378921c16",
+    "url": "/game-of-motivation/static/js/runtime-main.f6d1806a.js"
+  }
+]);
